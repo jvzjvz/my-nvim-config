8f13d4cab4259b62cc755d98cafd94752842b198
jvzjvz <154752793+JCodV@users.noreply.github.com>
2026-05-15 16:29:40 -0400

    neovide config

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+if vim.g.neovide == true then
+  vim.o.guifont = 'JetBrainsMono Nerd Font Mono :h16'
+  -- vim.o.guifont = 'LythMono Nerd Font :h16:b'
+  -- vim.o.guifont = 'FiraCode Nerd Font :h16'
+  -- vim.o.guifont = 'Hack Nerd Font Mono :h16'
+  vim.api.nvim_set_keymap("n", "<C-=>", ":lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  1.0)<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + 0.05, 1.0)<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-_>", ":lua vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - 0.05, 0.0)<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 0.5<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-)>", ":lua vim.g.neovide_transparency = 0.9<CR>", { silent = true })
+end
+
 vim.g.mapleader = ' '
 vim.g.localleader = ' '
 vim.opt.timeoutlen = 300
